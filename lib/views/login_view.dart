f83e42e446dad6a8a2703bd2833950857816e6ce
Deepu <93904807+dvdeepu99@users.noreply.github.com>
2023-12-17 15:46:43 +0530

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firstapp/constants/routes.dart';
-import 'package:firstapp/firebase_options.dart';
-import 'package:firstapp/views/verify_email_view.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
